d414632b65b4c3ab163dc7242cf4fd0fff31c66f
LizaDenya <71431586+LizaDenya@users.noreply.github.com>
2020-10-10 23:10:14 +0300

    Add files via upload

---
+++ 4.5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	float a, b, s, r, p, c;
+	cin >> a >> b;
+	s = abs(a) + abs(b);
+	r = abs(a) - abs(b);
+	p = abs(a) * abs(b);
+	c = abs(a) / abs(b);
+	cout << s << " " << r << " " << p << " " << c;
+	return 0;
+}
